890d26b0a8ad971bd9c6bd4f12727dd5f75ac910
Musab Adam <musabadam95@gmail.com>
2025-04-14 16:05:14 +0100

    fix: renamed files

---
+++ .history/src/App_20250414155858.tsx
@@ -0,0 +1,10 @@
+import GreenStatus from "./components/GreenEnergyPercentage/GreenEnergyPercentage";
+function App() {
+  return (
+    <div id="template-text">
+      <GreenStatus/>
+    </div>
+  );
+}
+
+export default App;
